61055658256d009b42194ef477e01914aad4095a
CyrilPonsan <cyril.ponsan@gmail.com>
2022-12-18 11:13:24 +0100

    113

--- server/src/server.js
+++ server/src/server.js
@@ -1,5 +1,18 @@
-const express = require("express");
+const http = require("http");
+
+const app = require("./app");
+const { loadPlanetsData } = require("./models/planets.model");
 
 const PORT = process.env.PORT || 8000;
 
-const app = express();
+const server = http.createServer(app);
+
+async function startServer() {
+  await loadPlanetsData();
+
+  server.listen(PORT, () => {
+    console.log(`Listening on port : ${PORT}...`);
+  });
+}
+
+startServer();
